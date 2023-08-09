b3c50c938c940a0c077f0e7a4d05e2f6a22d91ee
unknown <lucas.hsilva@icloud.com>
2023-08-09 13:54:55 -0300

    decisoes

---
+++ decisoes.php
@@ -0,0 +1,28 @@
+<?php
+
+    $idade = 15;
+    $numeroDePessoas = 2;
+
+    echo "Você só pode entrar se tiver a partir de 18 anos ou ";
+    echo "a partir de 16 anos acompanhado" . PHP_EOL;
+
+     //($idade == 18 || $idade > 18) 
+     //($idade == 18 or $idade > 18) 
+     //($idade >= 18 and $nome == 'Lucas' )
+     //($idade >= 18 && $nome == 'Lucas' )
+
+    if ($idade >= 18)
+     
+        {
+            echo "Você tem $idade anos. Pode entrar sozinho";
+
+    }else if ($idade >= 16 && $numeroDePessoas > 1){
+            echo "Você tem $idade anos, está acompanhado(a), então pode entrar.";
+    }else{
+            echo "Você só tem $idade anos. Você não pode  entrar.";
+         }
+        
+
+        echo PHP_EOL;
+        echo "ADEUS!!!!";
+?>
