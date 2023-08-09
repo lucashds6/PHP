ea377e91be91305b690de30996bf2bd2a0fd2eeb
unknown <lucas.hsilva@icloud.com>
2023-08-08 23:36:25 -0300

    alura

---
+++ operacoes.php
@@ -0,0 +1,9 @@
+<?php
+
+$idade = 21;
+$idadeDaqui10Anos = $idade + 10;
+
+echo $idadeDaqui10Anos;
+
+
+?>
---
+++ strings.php
@@ -0,0 +1,7 @@
+<?php 
+
+$idade = 21;
+
+echo "Olá Mundo!" . PHP_EOL; //quebra de linha
+echo "Olá Mundo! Minha idade é $idade anos";
+?>
