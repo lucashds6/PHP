d38087149da2fb8ab23f085cf78daf4c239455fa
unknown <lucas.hsilva@icloud.com>
2023-08-14 23:17:04 -0300

    alura

---
+++ avancando.php/funcoes.php
@@ -0,0 +1,32 @@
+<?php
+exibeMensagem(mensagem:"Olá Arquivo de funções aqui.");
+
+function sacar(array $conta, float $valorASacar) : array
+{
+    if ($valorASacar > $conta ['saldo']) {
+        exibeMensagem(mensagem: "Você não pode sacar");
+   } else {
+    $conta ['saldo'] -= $valorASacar;
+   }
+
+   return $conta;
+}
+
+function exibeMensagem(string $mensagem)
+{
+    echo $mensagem . PHP_EOL;
+}
+
+function depositar(array $conta, float $valorADepositar) : array
+{
+    if ($valorADepositar > 0){
+        $conta['saldo'] += $valorADepositar;
+    } else {
+        exibeMensagem(mensagem:"Depositos precisam ser positivos");
+    }
+    
+    return $conta;
+    
+}
+
+?>
