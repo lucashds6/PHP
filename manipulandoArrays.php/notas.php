e9d5df5b38a3e2de8b23b7cb492b6b191b97f5f7
unknown <lucas.hsilva@icloud.com>
2023-08-30 17:46:52 -0300

    alura

--- manipulandoArrays.php/notas.php
+++ manipulandoArrays.php/notas.php
@@ -1,15 +1,30 @@
 <?php
 
 $notas = [
-    'Vinicius' => 6,
+    'Vinicius' => null,
     'João'=> 8,
     'Ana' =>10,
     'Roberto' => 7,
-    'Maria' => 9
-    
+    'Maria' => 9 
 ];
 
 krsort($notas);
 var_dump($notas);
+
+if(is_array($notas)){
+    echo 'Sim, é um array' . PHP_EOL;
+}
+
+var_dump(array_is_list($notas));
+
+echo 'Vinicius fez a prova:' . PHP_EOL;
+var_dump(isset($notas['Ana']));
+
+echo 'Alguém tirou 6?' . PHP_EOL;
+var_dump(in_array(6, $notas));
+
+//array_key_exists = verifidca se a chave existe
+//in_array = verifica se o valor existe
+//isset = verifica se a chave existe e não é nula
 
 ?>
